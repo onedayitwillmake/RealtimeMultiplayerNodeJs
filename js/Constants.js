60eb6a87a235f016eca21e7e42e19798b3bad124
onedayitwillmake <mariogonzalez@gmail.com>
2011-04-22 13:53:28 -0400

    Added Stats.js

--- js/Constants.js
+++ js/Constants.js
@@ -16,6 +16,15 @@ Version:
 */
 (function(){
 	RealtimeMultiplayerGame.Constants = {
-		ENTITY_DEFAULT_RADIUS: 10
+		ENTITY_DEFAULT_RADIUS: 10,
+
+		CMDS: {
+			PLAYER_CONNECT		: 1 << 0,			// Initial connection to the server, not in game yet
+			SERVER_MATCH_START	: 1 << 1,			// Server broadcast game start
+			SERVER_END_GAME		: 1 << 2,			// Server broadcast game over
+			PLAYER_JOINED		: 1 << 3,           // Player has joined the current game
+			PLAYER_DISCONNECT	: 1 << 4,           // Player has disconnected
+			PLAYER_UPDATE		: 1 << 5			// Player is sending sampled input
+		}
 	}
 })();
